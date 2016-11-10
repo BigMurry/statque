6808c69bd0a7f8877c0757509a4a35c5ccd636af
bigmurry <hhdiao@siom.ac.cn>
2016-11-10 16:18:08 +0800

    fix test for old version node

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
 
-gulp.task('default', function (cb) {
-  return gulp.src('src/**')
+gulp.task('transformTest', function (cb) {
+  return gulp.src('test/**/*')
+             .pipe(babel({presets: ['es2015']}))
+             .pipe(gulp.dest('__test__'))
+})
+gulp.task('release', function (cb) {
+  return gulp.src('src/**/*')
              .pipe(babel({presets: ['es2015']}))
              .pipe(gulp.dest('lib'))
 })
+
+gulp.task('default', ['transformTest', 'release'])
